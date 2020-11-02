c3e1d13fe4b38479906e71bc8eea4f8b69c742f8
Franklyn javascript <ajifrank75@gmail.com>
2020-11-02 17:34:02 +0100

    Initial commit to this repo

---
+++ fb_test.js
@@ -0,0 +1,29 @@
+function setup() {                                  // Declare function
+    var textInput;                                    // Create variable
+    textInput = document.getElementById('firstname');  // Get username input
+    textInput.focus();                                // Give username focus
+}
+
+window.addEventListener('load', setup, false); // When page loaded call setup()
+
+function checkUsername() {                        // Declare function
+var username = el.value;                        // Store username in variable
+if (username.length < 5) {                      // If username < 5 characters
+    elMsg.className = 'warning';                  // Change class on message
+    elMsg.textContent = 'Not long enough, yet...';// Update message
+} else {                                        // Otherwise
+    elMsg.textContent = '';                       // Clear the message
+}
+}
+
+function tipUsername() {                          // Declare function
+elMsg.className = 'tip';                        // Change class for message
+elMsg.innerHTML = 'Username must be at least 5 characters'; // Add message
+}
+
+var el = document.getElementById('username');     // Username input
+var elMsg = document.getElementById('feedback');  // Element to hold message
+
+// When the username input gains / loses focus call functions above:
+el.addEventListener('focus', tipUsername, false); // focus call tipUsername()
+el.addEventListener('blur', checkUsername, false);// blur call checkUsername()
